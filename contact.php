e8f21c54a40c57b13a2cf3566457dda0874aaced
jeanalb <jeanalb@sfr.fr>
2018-04-13 14:40:32 +0200

    changement page contact
    bref

--- contact.php
+++ contact.php
@@ -4,6 +4,28 @@
 	<title>pagejean</title>
 </head>
 <body>
-<h1>Titre Contact</h1>
+<h1>Formulaire de Contact</h1>
+
+<form>
+  E-mail:
+  <input type="email" name="email">
+
+   Telephone:
+  <input type="tel" name="usrtel"><br><br>
+
+  <textarea name="message" rows="10" cols="60">
+	Vos commentaires
+</textarea><br><br>
+
+<h3>Voulez-vous recevoir des SMS de notre part.</h3>
+
+  <input type="radio" name="gender" value="oui" checked>OUI<br><ber>
+  <input type="radio" name="gender" value="non">NON<br><br>
+
+  <button type="button" onclick="alert('Merci')">envoyer</button>
+</form><br><br>
+
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38944.43428776704!2d5.371779150379335!3d43.49060203766503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9ed54c01d4d4d%3A0x2b7de27c1ed474ad!2s6+Les+Rives+Saint-Joseph!5e0!3m2!1sfr!2sfr!4v1523622997395" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 </body>
 </html>
