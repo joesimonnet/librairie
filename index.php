8584d8232d0522b1b171afb8c6f21672629ca357
joesimonnet <joesimonnet@gmail.com>
2018-04-13 13:39:23 +0200

    ajout de la page index

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Librairie Mot à Mot</title>
+</head>
+<body>
+<header>
+<img src="public/assets/img/logo.jpg" alt="logo librairie">
+<h1>Librairie Mot à Mot</h1>
+<nav>
+<ul>
+    <li><a href="#">accueil</a></li>
+    <li><a href="#">Coups de coeurs</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Inscription</a></li>
+    <li><a href="#">Connexion</a></li>
+    <li><a href="#">Administration</a></li>
+</ul>
+<div class="search"> BARRE DE RECHERCHE <a href="#">Avancé</a></div>
+</nav>
+</header>
+
+<figure>
+    <img src="public/assets/img/facade.jpg" alt="Facade librairie">
+    <figcaption>
+        68 place Mirabeau 84120 PERTUIS<br> 
+        04 90 79 02 04 ou 06 30 78 38 65<br>
+        lien facebook / lien Instagram
+    </figcaption>
+</figure>
+
+<div class="actus">
+    <h2>Mon actu</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro autem laudantium dolore sint. Inventore, suscipit ea tempora maxime officiis dignissimos!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis similique accusamus cumque repellendus ipsa voluptas aliquid dolores, nihil qui voluptatem molestias, perspiciatis saepe illum quas necessitatibus iste assumenda libero error. Est dicta non magni perferendis saepe ullam velit delectus voluptatibus.</p>
+</div>
+
+<footer>
+<small>&copy; 2018 - tous droits réservés - Librairie Mots à Mots - 68 place Mirabeau 84120 PERTUIS </small>
+</footer>
+
+
+    
+</body>
+</html>
